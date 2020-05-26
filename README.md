6109e71d3e337ea080eed336ba0e4bc5fa21f381
Tristan Rivoallan <tristan@rivoallan.net>
2020-05-26 15:13:39 +0200

    UN PORN SANS BÉNÉF

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fr.partouze-cagoule.kneeporn
+
+Sans conteste le plus beau site que j'ai jamais codé.
---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+$lines = file(__DIR__.'/citations.txt');
+$text = $lines[array_rand($lines)];
+?>
+<html>
+	<!-- C'est sans conteste le plus beau site que j'ai jamais programmé -->
+	<head>
+		<title>Knee Porn</title>
+		<style>
+			@font-face {
+				font-family: "Trickster";
+ 				src: url("/Trickster-master/fonts/Trickster-Reg.ttf");
+			}
+			html {
+ 				background: url(kneeporn.png) no-repeat center center fixed; 
+ 				-webkit-background-size: cover;
+ 				-moz-background-size: cover;
+ 				-o-background-size: cover;
+  				background-size: cover;
+			}
+			body {
+				text-align: center;
+				font-size: 3em;
+				cursor: url(unmessagesimple.cur), pointer;
+			}
+			h1 {
+				color: pink;
+				font-family: Trickster;
+				margin: 0;
+			}
+			p {
+				font-size: 0.3em;
+			}
+			h3 {
+				color: lime;
+			}
+			cursor: url(hand.cur), pointer;
+		</style>
+		<script>
+			var answer = window.prompt('Tapez "GENOU" pour continuer');
+			if (answer == 'PROUT') {
+				alert('VOUS ÊTES INTOLÉRANTE AU LACTOSE');
+			}
+		</script>
+		<meta property="og:description" content="Pour une vengeance rétroactive de 4000 ans" />
+	</head>
+
+	<body>
+		<a href="https://www.youtube.com/channel/UCpXb2DVqdX5MzF9On5Gx8yQ/videos">
+			<h1>Welcome to my page</h1>
+		</a>
+
+
+		<h3><?php echo $text ?></h3>
+
+		
+		<a href="manifesto.png">
+			<img src="gloireaugenou.gif" />
+		</a>
+
+		<p>
+			SEND <strong>KNUDES</strong> AND I SEND YOU <strong>KMINES<strong> :-*
+		</p>
+		<p>
+			<a href="mailto:kneeporn@partouze-cagoule.fr">kneeporn@partouze-cagoule.fr</a>
+		</p>
+	</body>
+</html>²
